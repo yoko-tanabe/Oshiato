d0b3a906e338f185b0f6b0d8970d8773821b9cee
yoko-tanabe <yoko.tanabe.213@gmail.com>
2026-04-04 14:20:14 +0900

    Uint8ClampedArray の型定義を変更

--- apps/web/types/heic-decode.d.ts
+++ apps/web/types/heic-decode.d.ts
@@ -2,7 +2,7 @@ declare module 'heic-decode' {
   interface DecodeResult {
     width: number;
     height: number;
-    data: Uint8ClampedArray;
+    data: Uint8ClampedArray<ArrayBuffer>;
   }
   function decode(input: { buffer: Uint8Array }): Promise<DecodeResult>;
   export default decode;
